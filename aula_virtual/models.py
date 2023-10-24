98b999987fee2d666c0be8d7697eadef1840b0f5
Facundo Otero <facu_otero@hotmail.com>
2023-10-23 23:18:17 -0300

    correccion de aula_virtual

--- aula_virtual/models.py
+++ aula_virtual/models.py
@@ -1,10 +1,11 @@
+from django.db import models
 
+# Create your models here.
 from django.db import models
 
 
 class AulaVirtual(models.Model):
     id_alumno = models.ForeignKey("alumno.Alumno", on_delete=models.CASCADE)
-    id_profesor = models.ForeignKey("profesor.Profesor", on_delete=models.CASCADE)
-    #id_materia = models.ForeignKey("materia.Materia", on_delete=models.CASCADE)
+    id_profesor = models.ForeignKey("profesor.Profesor", on_delete=models.CASCADE)   
     fecha = models.DateField()
     
--- profesor/models.py
+++ profesor/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 class Profesor(User):
     documento= models.CharField(max_length=100, unique=True)
-    phone_number = models.IntegerField("Número de Teléfono", max_length=15, default=0)
-    phone_code = models.IntegerField("Código de Teléfono", max_length=3, default=0)
+    phone_number = models.IntegerField("Número de Teléfono", max_length=15)
+    phone_code = models.IntegerField("Código de Teléfono", max_length=3)
     #valoracion = models.ForeignKey(RankingProfesor, on_delete=models.CASCADE)
