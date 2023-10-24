a915fa4f7197fa3fce7c8f503bef6471c71412c2
Isaac-ADL <isaac_adl@outlook.es>
2023-10-23 19:38:33 -0600

    vistas del modulo de alumnos

--- alumno/views.py
+++ alumno/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
-# Create your views here.
+from .models import Alumno
+from .forms import AlumnoRegistrationForm
+from django.urls import reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
 
 def hola(request):
     return HttpResponse("Hola Alumno!")
+
+def success(request):
+    return HttpResponse("Exito!")
+    
+class AlumnoRegistrationView(CreateView):
+    model = Alumno
+    form_class = AlumnoRegistrationForm
+    template_name = 'signup.html'  # Crea un template de registro
+    success_url = reverse_lazy('alumnos:success')
