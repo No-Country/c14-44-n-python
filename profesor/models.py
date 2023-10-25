0cdceaeed0b3d3c69818e2de58f866f1bacbea21
Isaac-ADL <isaac_adl@outlook.es>
2023-10-24 19:15:40 -0600

    Modelo actualizado con validadores

--- profesor/models.py
+++ profesor/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Profesor(User):
     #nombre = models.CharField(max_length=100)
     #apellido = models.CharField(max_length=100)
     documento= models.CharField(max_length=100, unique=True)
-<<<<<<< HEAD
     #rol = models.ForeignKey(AulaVirtual, on_delete=models.CASCADE)
     #email = models.EmailField(unique=True)
-    telefono = models.CharField(max_length=20)
-=======
-    phone_number = models.IntegerField("Número de Teléfono", max_length=15)
-    phone_code = models.IntegerField("Código de Teléfono", max_length=3)
->>>>>>> 14822ed5dc4ac050e84dab0e358ed15285ee1a02
+    #telefono = models.CharField(max_length=20)
+    phone_number = models.IntegerField("Número de Teléfono", default=1000000000, validators = [
+        MaxValueValidator(999999999999999),
+        MinValueValidator(1000000000)
+    ])
+    phone_code = models.IntegerField("Código de Teléfono", default=10, validators = [
+        MaxValueValidator(999),
+        MinValueValidator(10)
+    ])
     #valoracion = models.ForeignKey(RankingProfesor, on_delete=models.CASCADE)
