a7705c05336e8af898f85de1393ca14dcc638882
Isaac-ADL <isaac_adl@outlook.es>
2023-10-24 18:49:26 -0600

    Actualizadas urls

--- study_helper/urls.py
+++ study_helper/urls.py
@@ -1,12 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from study_helper import views
+from study_helper.views import Index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', Index.as_view(), name='index'),
     path('alumnos/', include('alumno.urls'))
 ]
 
