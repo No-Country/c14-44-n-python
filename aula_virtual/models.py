4a1762aeec4b0bf077a35d09d2b09f0e93b55a80
Facundo Otero <facu_otero@hotmail.com>
2023-10-31 21:20:52 -0300

    Cambie el aula virtual, le agregue dos direcciones al chat y el material se agrega no como un file sino como char

--- aula_virtual/models.py
+++ aula_virtual/models.py
@@ -7,7 +7,7 @@ class AulaVirtual(models.Model):
     id_alumno = models.ForeignKey("alumno.Alumno", on_delete=models.CASCADE)
     id_profesor = models.ForeignKey("profesor.Profesor", on_delete=models.CASCADE)   
     nombre = models.CharField("nombre", max_length=50)
-    material = models.FileField('material',)
+    material = models.CharField('material',max_length=10000)
     fecha = models.DateField("fecha_de_clase",)
     def __str__(self):
         return f'Aula: {self.nombre} - Fecha: {self.fecha}'
